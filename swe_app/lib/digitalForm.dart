385de7b4962c764b9d574dab73d6a8d74e9a78a5
rmac3392 <ryanjamesmacawili@gmail.com>
2023-04-19 09:31:45 +0800

    next route update

--- swe_app/lib/digitalForm.dart
+++ swe_app/lib/digitalForm.dart
@@ -1,48 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class MydDgitalForm extends StatefulWidget {
-  const MydDgitalForm({super.key});
+class MyDigitalForm extends StatefulWidget {
+  const MyDigitalForm({super.key});
   @override
-  State<MydDgitalForm> createState() => _MyAppState();
+  State<MyDigitalForm> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MydDgitalForm> {
+class _MyAppState extends State<MyDigitalForm> {
   @override
   // TODO: implement widget
-  Widget build (BuildContext context){
-    return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            leading: Container(
-            margin: const EdgeInsets.all(3),
-            child: Image.asset(
-              "images/logo.png",
-              height: 50,
-              width: 50,
-            ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(
+          margin: const EdgeInsets.all(3),
+          child: Image.asset(
+            "images/logo.png",
+            height: 50,
+            width: 50,
           ),
-          title: (Text(
-              "S.W.C.S",
-              style: GoogleFonts.notoSerif(
-                fontSize: 35,
-                fontWeight: FontWeight.bold,
-                textStyle: const TextStyle(fontStyle: FontStyle.italic),
-              ),
-            )),
-          actions: [
-            Container(
-              margin: const EdgeInsets.all(5),
-            child: const  Icon(
+        ),
+        title: (Text(
+          "S.W.C.S",
+          style: GoogleFonts.notoSerif(
+            fontSize: 35,
+            fontWeight: FontWeight.bold,
+            textStyle: const TextStyle(fontStyle: FontStyle.italic),
+          ),
+        )),
+        actions: [
+          Container(
+            margin: const EdgeInsets.all(5),
+            child: const Icon(
               Icons.menu_rounded,
               size: 32,
-              ),
             ),
-          ],
-          backgroundColor: const Color(0xFF0F172A),
           ),
-          body: Column(),
-        ),
-      );
-  } 
+        ],
+        backgroundColor: const Color(0xFF0F172A),
+      ),
+      body: Column(),
+    );
+  }
 }
--- swe_app/lib/main.dart
+++ swe_app/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'digitalForm.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false, 
+    home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -12,140 +15,141 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-
 class _MyAppState extends State<MyApp> {
   MobileScannerController cameraController = MobileScannerController();
   bool _screenOpened = false;
-  void foundBarCode(Barcode barcode, MobileScannerArguments? args) {
-    if (!_screenOpened) {
-      final String code = barcode.rawValue ?? "---";
-      debugPrint("Barcode found! $code");
-      _screenOpened = true;
-    }
-  }
-
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Container(
-            margin: const EdgeInsets.all(3),
-            child: Image.asset(
-              "images/logo.png",
-              height: 50,
-              width: 50,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(
+          margin: const EdgeInsets.all(3),
+          child: Image.asset(
+            "images/logo.png",
+            height: 50,
+            width: 50,
           ),
-          title: (Text(
-              "S.W.C.S",
-              style: GoogleFonts.notoSerif(
-                fontSize: 35,
-                fontWeight: FontWeight.bold,
-                textStyle: const TextStyle(fontStyle: FontStyle.italic),
-              ),
-            )),
-          actions: [
-            Container(
-              margin: const EdgeInsets.all(5),
-            child: const  Icon(
+        ),
+        title: (Text(
+          "S.W.C.S",
+          style: GoogleFonts.notoSerif(
+            fontSize: 35,
+            fontWeight: FontWeight.bold,
+            textStyle: const TextStyle(fontStyle: FontStyle.italic),
+          ),
+        )),
+        actions: [
+          Container(
+            margin: const EdgeInsets.all(5),
+            child: const Icon(
               Icons.menu_rounded,
               size: 32,
+            ),
+          ),
+        ],
+        backgroundColor: const Color(0xFF0F172A),
+      ),
+      body: MobileScanner(
+        controller: cameraController,
+        onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          for (final barcode in barcodes) {
+            debugPrint('Barcode found! ${barcode.rawValue}');
+          }
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: const Color(0xFF0F172A),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 100,
+                child: IconButton(
+                  icon: ValueListenableBuilder(
+                    valueListenable: cameraController.torchState,
+                    builder: (context, state, child) {
+                      switch (state as TorchState) {
+                        case TorchState.off:
+                          return const Icon(
+                            Icons.toggle_off_rounded,
+                            size: 50,
+                            color: Color(0xFFFEFEFE),
+                          );
+                        case TorchState.on:
+                          return const Icon(
+                            Icons.toggle_on_rounded,
+                            size: 50,
+                            color: Color(0xFFFEFEFE),
+                          );
+                      }
+                    },
+                  ),
+                  iconSize: 42.0,
+                  onPressed: () => cameraController.toggleTorch(),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                height: 150,
+                child: IconButton(
+                  onPressed: () {
+                    void foundBarCode(
+                        Barcode barcode, MobileScannerArguments? args) {
+                      if (!_screenOpened) {
+                        final String code = barcode.rawValue ?? "---";
+                        debugPrint("Barcode found! $code");
+                        _screenOpened = true;
+                      }
+                    }
+
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: ((context) => const MyDigitalForm())));
+                    setState(() {
+                      print("clicked camera");
+                    });
+                  },
+                  icon: const Icon(
+                    Icons.camera,
+                    size: 100,
+                    color: Color(0xFFFEFEFE),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                height: 150,
+                child: IconButton(
+                  icon: ValueListenableBuilder(
+                    valueListenable: cameraController.cameraFacingState,
+                    builder: (context, state, child) {
+                      switch (state as CameraFacing) {
+                        case CameraFacing.front:
+                          return const Icon(
+                            Icons.cameraswitch_rounded,
+                            size: 47,
+                            color: Color(0xFFFEFEFE),
+                          );
+                        case CameraFacing.back:
+                          return const Icon(
+                            Icons.cameraswitch_rounded,
+                            size: 47,
+                            color: Color(0xFFFEFEFE),
+                          );
+                      }
+                    },
+                  ),
+                  iconSize: 42.0,
+                  onPressed: () => cameraController.switchCamera(),
+                ),
               ),
             ),
           ],
-          backgroundColor: const Color(0xFF0F172A),
-        ),
-        body: MobileScanner(
-          controller: cameraController,
-          fit: BoxFit.fitWidth,
-          onDetect: (capture) {
-            final List<Barcode> barcodes = capture.barcodes;
-            for (final barcode in barcodes) {
-              debugPrint('Barcode found! ${barcode.rawValue}');
-            }
-          },
-        ),
-        bottomNavigationBar: BottomAppBar( color: const Color(0xFF0F172A),
-          child: Row(
-            children: [
-              Expanded(
-                child: Container(
-                  height: 100,
-                  child: IconButton(
-                    icon: ValueListenableBuilder(
-                      valueListenable: cameraController.torchState,
-                      builder: (context, state, child) {
-                        switch (state as TorchState) {
-                          case TorchState.off:
-                            return const Icon(
-                              Icons.toggle_off_rounded,
-                              size: 50,
-                              color: Color(0xFFFEFEFE),
-                            );
-                          case TorchState.on:
-                            return const Icon(
-                              Icons.toggle_on_rounded,
-                              size: 50,
-                              color: Color(0xFFFEFEFE),
-                            );
-                        }
-                      },
-                    ),
-                    iconSize: 42.0,
-                    onPressed: () => cameraController.toggleTorch(),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  height: 150,
-                  child: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        print("clicked camera");
-                      });
-                    },
-                    icon: const Icon(
-                      Icons.camera,
-                      size: 100,
-                      color: Color(0xFFFEFEFE),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  height: 150,
-                  child: IconButton(
-                    icon: ValueListenableBuilder(
-                      valueListenable: cameraController.cameraFacingState,
-                      builder: (context, state, child) {
-                        switch (state as CameraFacing) {
-                          case CameraFacing.front:
-                            return const Icon(
-                              Icons.cameraswitch_rounded,
-                              size: 47,
-                              color: Color(0xFFFEFEFE),
-                            );
-                          case CameraFacing.back:
-                            return const Icon(
-                              Icons.cameraswitch_rounded,
-                              size: 47,
-                              color: Color(0xFFFEFEFE),
-                            );
-                        }
-                      },
-                    ),
-                    iconSize: 42.0,
-                    onPressed: () => cameraController.switchCamera(),
-                  ), 
-                ),
-              ),
-            ],
-          ), 
         ),
       ),
     );
